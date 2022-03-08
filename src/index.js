e0110273f5180d254da8644b8eb96c0972f5b0b5
Muniba Ahmed <mahmed93@berkeley.edu>
2022-03-08 13:15:03 -0800

    2/10

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-import './less/index.less'
+import "./less/index.less";
 
 // Your code goes here!
+
+// 1: Load
+window.onload = function (event) {
+  console.log(` event ${event.type} fired! Ready to rumble!`);
+  const heading = document.querySelector("h1");
+  heading.textContent = "Lets get this Party Started!";
+
+  //2: Key down
+  window.addEventListener("keydown", () => {
+    alert("GET YOUR RAGGEDY HANDS OFF MY SCREEN!!!");
+  });
+};
