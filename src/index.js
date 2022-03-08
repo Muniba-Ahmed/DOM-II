31e179121da7674769ef5874ac7914dd6ecfd20b
Muniba Ahmed <mahmed93@berkeley.edu>
2022-03-08 14:13:22 -0800

    completed 5 events

--- src/index.js
+++ src/index.js
@@ -2,14 +2,34 @@ import "./less/index.less";
 
 // Your code goes here!
 
-// 1: Load
+// Load
 window.onload = function (event) {
   console.log(` event ${event.type} fired! Ready to rumble!`);
   const heading = document.querySelector("h1");
-  heading.textContent = "Lets get this Party Started!";
+  heading.textContent = " Fun Bus Partayyyy!";
 
-  //2: Key down
+  // Key down
   window.addEventListener("keydown", () => {
-    alert("GET YOUR RAGGEDY HANDS OFF MY SCREEN!!!");
+    alert(
+      "THERES NOTHING MORE TO SEE. GET YOUR RAGGEDY HANDS OFF MY SCREEN!!!"
+    );
   });
 };
+
+//Copy
+window.addEventListener("copy", () => {
+  alert("THIS IS INTELLECTUAL PROPERTY THEFT! STOP STEALING MY STUFF BOZO");
+  console.log("stealing is bad ):");
+});
+
+//Click
+document.body.addEventListener("click", (event) => {
+  event.target.style.color = "limegreen";
+});
+
+// Double-Click
+document.body.addEventListener("dblclick", (event) => {
+  event.target.classList.toggle("mirror");
+});
+
+//Mouse Move & Mouse Over
